4ddf7cace9e63bb600ea76d8e243fbeb8fe5a66c
Nhat Thanh <thanhdev2003@example.com>
2022-05-14 08:56:29 +0700

    fix response

--- index.js
+++ index.js
@@ -12,31 +12,43 @@ const server = http.createServer(app);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api/v1/", router);
+app.get("/", function(req, res) {
+    return res.send("helloooooooooooooooooo");
+});
+app.use(
+    "/api/v1/",
+    function(req, res, next) {
+        console.log(req.body);
+        console.log(req.params);
+        console.log(req.query);
+        next()
+    },
+    router
+);
 app.get("/image/:id", (req, res) => {
-  Image.findOne({ _id: req.params.id })
-    .exec()
-    .then((image) => {
-      if (image) {
-        const buffer = image.data;
-        const contentType = image.contentType;
-        res.contentType(contentType || "image/png");
-        res.send(buffer);
-      } else {
-        res.status(403).send("Image not exist !");
-      }
-    })
-    .catch((err) => {
-      res.status(403).send(err.message);
-    });
+    Image.findOne({ _id: req.params.id })
+        .exec()
+        .then((image) => {
+            if (image) {
+                const buffer = image.data;
+                const contentType = image.contentType;
+                res.contentType(contentType || "image/png");
+                res.send(buffer);
+            } else {
+                res.status(403).send("Image not exist !");
+            }
+        })
+        .catch((err) => {
+            res.status(403).send(err.message);
+        });
 });
 server.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-  connectToMongodb()
-    .then(() => {
-      console.log("database connected");
-    })
-    .catch((err) => {
-      console.error(err.message);
-    });
+    console.log(`listening on port ${process.env.PORT}`);
+    connectToMongodb()
+        .then(() => {
+            console.log("database connected");
+        })
+        .catch((err) => {
+            console.error(err.message);
+        });
 });
--- src/middleware/sale.js
+++ src/middleware/sale.js
@@ -1,89 +1,95 @@
 import * as validate from "../../validate/validate.js";
 export const mPermission = (req, res, next) => {
-  if (req.body.payload.permission == "sales manager") {
-    next();
-  } else {
-    return res
+    if (req.body.payload.permission == "sales manager") {
+        next();
+    } else {
+        return res
 
-      .json({ success: false, result: "Bạn không thể sử dụng chức năng này" });
-  }
+            .json({ success: false, result: "Bạn không thể sử dụng chức năng này" });
+    }
 };
 export const mCreateBill = (req, res, next) => {
-  if (
-    validate.validateString(req.body.customer) &&
-    req.body.product.length > 0
-  ) {
-    req.body.bill = {
-      customer: req.body.customer,
-      product: req.body.product,
-      shop: req.body.payload.shop,
-    };
-    if (req.body.voucher) {
-      req.body.bill.voucher = req.body.voucher;
-    }
-    next();
-  } else {
-    return res
+    if (
+        validate.validateString(req.body.customer) &&
+        req.body.product
+    ) {
+        if (req.body.product.length > 0) {
+            req.body.bill = {
+                customer: req.body.customer,
+                product: req.body.product,
+                shop: req.body.payload.shop,
+            };
+            if (req.body.voucher) {
+                req.body.bill.voucher = req.body.voucher;
+            }
+            next();
+        } else {
+            return res
 
-      .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
-  }
+                .json({ success: false, result: "Dach sách sản phẩm còn trống" });
+        }
+    } else {
+        return res
+
+            .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
+    }
 };
 export const mCreateCustomer = (req, res, next) => {
-  if (
-    validate.validateCardNumber(req.body.code) &&
-    validate.validateString(req.body.name) &&
-    validate.validateString(req.body.address) &&
-    validate.validatePhone(req.body.phone)
-  ) {
-    let customer = {
-      name: req.body.name,
-      address: req.body.address,
-      cardID: {
-        code: req.body.code,
-      },
-      phone: req.body.phone,
-      shop: req.body.payload.shop,
-    };
-    req.body.customer = customer;
-    next();
-  } else {
-    return res
- 
-      .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
-  }
+    if (
+        validate.validateCardNumber(req.body.code) &&
+        validate.validateString(req.body.name) &&
+        validate.validateString(req.body.address) &&
+        validate.validatePhone(req.body.phone)
+    ) {
+        let customer = {
+            name: req.body.name,
+            address: req.body.address,
+            cardID: {
+                code: req.body.code,
+            },
+            phone: req.body.phone,
+            shop: req.body.payload.shop,
+        };
+        req.body.customer = customer;
+        next();
+    } else {
+        return res
+
+            .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
+    }
 };
 export const mUpdateVoucherOfBilling = (req, res, next) => {
-  if (req.body.voucher) {
-    next();
-  } else {
-    return res
+    if (req.body.voucher) {
+        next();
+    } else {
+        return res
 
-      .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
-  }
+            .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
+    }
 };
 export const mCreateVoucher = (req, res, next) => {
-  if (req.body.conditionTotal && req.body.time) {
-    req.body.voucher = {
-      time: req.body.time,
-      conditionTotal: req.body.conditionTotal,
-      shop: req.body.payload.shop,
-    };
-    if (req.body.percent || req.body.money || req.body.description) {
-      if (req.body.description) {
-        req.body.voucher.description = req.body.description;
-      }
-      if (req.body.percent || req.body.money) {
-        if (req.body.percent) {
-          req.body.voucher.percent = req.body.percent;
-        } else {
-          req.body.voucher.money = req.body.money;
+    if (req.body.conditionTotal && req.body.time) {
+        req.body.voucher = {
+            time: req.body.time,
+            conditionTotal: req.body.conditionTotal,
+            shop: req.body.payload.shop,
+        };
+        if (req.body.percent || req.body.money || req.body.description) {
+            if (req.body.description) {
+                req.body.voucher.description = req.body.description;
+            }
+            if (req.body.percent || req.body.money) {
+                if (req.body.percent) {
+                    req.body.voucher.percent = req.body.percent;
+                } else {
+                    req.body.voucher.money = req.body.money;
+                }
+            }
         }
-      }
+        next();
+    } else {
+        return res
+
+            .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
     }
-    next();
-  } else {
-    return res
-  
-      .json({ success: false, result: "Dữ liệu yêu cầu còn thiếu" });
-  }
 };
