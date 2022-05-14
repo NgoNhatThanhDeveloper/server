610ee1a7c2190a17db4b4af427e0cd4c3ad0b8a9
Nhat Thanh <thanhdev2003@example.com>
2022-05-14 08:43:31 +0700

    fix response

--- src/service/sale.js
+++ src/service/sale.js
@@ -1,122 +1,122 @@
 import * as repository from "../repository/sale.js";
 export const createBillService = (req, res) => {
-  repository
-    .createBill(req.body.bill)
-    .then((bill) => {
-      return res.json({ result: "Yêu cầu thành công", data: bill });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .createBill(req.body.bill)
+        .then((bill) => {
+            return res.json({ success: true, result: "Yêu cầu thành công", data: bill });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const createCustomerService = (req, res) => {
-  repository
-    .createCustomer(req.body.customer, req.files)
-    .then((id_customer) => {
-      return res
-        
-        .json({ result: "Yêu cầu thành công", data: id_customer });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .createCustomer(req.body.customer, req.files)
+        .then((id_customer) => {
+            return res
+
+                .json({ success: true, result: "Yêu cầu thành công", data: id_customer });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const removeBillService = (req, res) => {
-  repository
-    .removeBill(req.params.id, req.body.payload.shop)
-    .then(() => {
-      return res.json({ result: "Yêu cầu thành công" });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .removeBill(req.params.id, req.body.payload.shop)
+        .then(() => {
+            return res.json({ success: true, result: "Yêu cầu thành công" });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const updateVoucherOfBillService = (req, res) => {
-  repository
-    .updateVoucherOfBill(req.params.id, req.body.payload.shop, req.body.voucher)
-    .then((bill) => {
-      return res.json({ result: "Yêu cầu thành công", data: bill });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .updateVoucherOfBill(req.params.id, req.body.payload.shop, req.body.voucher)
+        .then((bill) => {
+            return res.json({ success: true, result: "Yêu cầu thành công", data: bill });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const showBillService = (req, res) => {
-  req.query.shop = req.body.payload.shop;
-  repository
-    .showBill(req.query)
-    .then((bills) => {
-      return res
-        
-        .json({ success: true, result: "Yêu cầu thành công", data: bills });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    req.query.shop = req.body.payload.shop;
+    repository
+        .showBill(req.query)
+        .then((bills) => {
+            return res
+
+                .json({ success: true, result: "Yêu cầu thành công", data: bills });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const createVoucherService = (req, res) => {
-  repository
-    .createVoucher(req.body.voucher)
-    .then(() => {
-      return res
-        
-        .json({ success: true, result: "Yêu cầu thành công" });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .createVoucher(req.body.voucher)
+        .then(() => {
+            return res
+
+                .json({ success: true, result: "Yêu cầu thành công" });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const removeVoucherService = (req, res) => {
-  repository
-    .removeVoucher(req.params.id, req.body.payload.shop)
-    .then(() => {
-      return res
-        
-        .json({ success: true, result: "Yêu cầu thành công" });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .removeVoucher(req.params.id, req.body.payload.shop)
+        .then(() => {
+            return res
+
+                .json({ success: true, result: "Yêu cầu thành công" });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const showVoucherService = (req, res) => {
-  req.query.shop = req.body.payload.shop;
-  repository
-    .showVoucher(req.query)
-    .then((vouchers) => {
-      return res
-        
-        .json({ success: true, result: "Yêu cầu thành công", data: vouchers });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    req.query.shop = req.body.payload.shop;
+    repository
+        .showVoucher(req.query)
+        .then((vouchers) => {
+            return res
+
+                .json({ success: true, result: "Yêu cầu thành công", data: vouchers });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const updatePayment = (req, res) => {
-  repository
-    .updatePayment(
-      req.params.id,
-      req.body.payload.shop,
-      req.body.total,
-      req.body.paid
-    )
-    .then(() => {
-      return res
-        
-        .json({ success: true, result: "Yêu cầu thành công" });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    repository
+        .updatePayment(
+            req.params.id,
+            req.body.payload.shop,
+            req.body.total,
+            req.body.paid
+        )
+        .then(() => {
+            return res
+
+                .json({ success: true, result: "Yêu cầu thành công" });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
 export const showPaymentService = (req, res) => {
-  req.query.shop = req.body.payload.shop;
-  repository
-    .showPayment(req.query)
-    .then((payments) => {
-      return res
-        .json({ success: true, result: "Yêu cầu thành công", data: payments });
-    })
-    .catch((error) => {
-      return res.json({ success: false, result: error.message });
-    });
+    req.query.shop = req.body.payload.shop;
+    repository
+        .showPayment(req.query)
+        .then((payments) => {
+            return res
+                .json({ success: true, result: "Yêu cầu thành công", data: payments });
+        })
+        .catch((error) => {
+            return res.json({ success: false, result: error.message });
+        });
 };
